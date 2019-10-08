0a709f1894b18d2e41146a36222402fa6031a18b
Pontus <ponja383@student.liu.se>
2019-10-08 09:33:50 +0800

    homepage alpha version is done

---
+++ week_7/CaseStudyPt4/scripts/javascript/calcprice.js
@@ -0,0 +1,53 @@
+var priceArray = [0.0,0.0,0.0];
+
+function calculatePrice(id){
+    switch(id){
+        case 1:
+            priceArray[0] = 2.0*getQuantity(id);
+            document.getElementById("regularTotal").innerHTML = priceArray[0].toFixed(2);
+            break;
+        case 2:
+            if(document.getElementById("cafeLow").checked){
+                priceArray[1] = 2.0*getQuantity(id);
+            }
+            else {
+                priceArray[1] = 3.0*getQuantity(id);
+            }
+            document.getElementById("cafeTotal").innerHTML = priceArray[1].toFixed(2);
+            break;
+        case 3:
+            if(document.getElementById("cappuccinoLow").checked){
+                priceArray[2] = 4.75*getQuantity(id);
+            }
+            else{
+                priceArray[2] = 5.75*getQuantity(id);
+            }
+            document.getElementById("cappuccinoTotal").innerHTML = priceArray[2].toFixed(2);
+            break;
+    }
+    document.getElementById("totalPrice").innerHTML = calculateTotal().toFixed(2);
+}
+function calculateTotal(){
+    return (priceArray[0] + priceArray[1] + priceArray[2]);
+}
+function getQuantity(id){
+    switch(id){
+        case 1:
+            return document.getElementById("regularQuan").value;
+        case 2:
+            return document.getElementById("cafeQuan").value;
+        case 3:
+            return document.getElementById("cappuccinoQuan").value;
+    }
+}
+
+function placeOrder(){
+    alert(calculateTotal());
+}
+function init(){
+    for(i = 1; i <=3; i++){
+        calculatePrice(i);
+    }
+    calculateTotal();
+}
+window.onload=init;
