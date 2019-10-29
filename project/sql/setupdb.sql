8b341cf2c2aad31a544b1892330b7d5fb4b4e03f
Lukas Pohlman <lukpohl3@gmail.com>
2019-10-29 10:16:32 +0800

    setup db is perf

--- project/sql/setupdb.sql
+++ project/sql/setupdb.sql
@@ -1,15 +1,20 @@
 -- Category 1: Pizza; Category 2: Salad; Category 3: Drinks; Category 4: Desserts;
--- source C:\Users\lukpo\Dropbox\Project\ee4717\project\sql\setupdb.sql
+-- source C:\Apache24\htdocs\project\sql\setupdb.sql
 
-CREATE DATABASE f32ee;
-use f32ee;
+CREATE DATABASE IF NOT EXISTS f32ee;
+USE f32ee;
 
+SELECT "Dropping tables" AS MESSAGE;
+SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS de_applicants;
+DROP TABLE IF EXISTS de_category;
 DROP TABLE IF EXISTS de_products;
 DROP TABLE IF EXISTS de_sales;
 DROP TABLE IF EXISTS de_positions;
 DROP TABLE IF EXISTS de_employees;
+SET FOREIGN_KEY_CHECKS = 1;
 
+SELECT "Creating new tables" AS MESSAGE;
 CREATE TABLE de_category (
     category_id INT UNSIGNED PRIMARY KEY,
     category_name VARCHAR(30),
@@ -58,12 +63,14 @@ CREATE TABLE de_applicants (
     CONSTRAINT FOREIGN KEY fk_applicant(applied_for) REFERENCES de_positions(position_id)
 );
 
+SELECT "Filling category table" AS MESSAGE;
 INSERT INTO f32ee.de_category (category_id, category_name, category_description)
     VALUES (1, "Pizza", "All pizzas include durian, mozzarella, and tomato sauce."),
     (2, "Salad", "All salads include durian, iceberg lettuce, tomatoes, and cucumber."),
     (3, "Drink", "All drinks include durian and ice as standard."),
     (4, "Dessert", "All desserts include durian as standard.");
 
+SELECT "Filling product table" AS MESSAGE;
 INSERT INTO f32ee.de_products (product_id, product_name, product_price, product_description, product_category)
     VALUES (NULL, "The Original", 12.95, "Ham", 1),
     (NULL, "Tutti Frutti", 15.95, "Mango, Pineapple, Banana, Curry powder", 1),
@@ -78,11 +85,13 @@ INSERT INTO f32ee.de_products (product_id, product_name, product_price, product_
     (NULL, "Lemon Pudding", 4.95, "Lemon/Durian pudding",  4),
     (NULL, "Durian Pancake", 5.95, "Served with Durian Jam",4);
 
+SELECT "Filling position table" AS MESSAGE;
 INSERT INTO f32ee.de_positions (position_id, position_name, position_salary)
     VALUES (1, "Cook", 1000),
-    (2, "Manager", 1500);
+    (2, "Manager", 1500),
     (3, "Delivery Driver", 750);
 
+SELECT "Filling employee table" AS MESSAGE;
 INSERT INTO f32ee.de_employees (ssn, first_name, last_name, pos)
     VALUES (9212281234, "Luigi", "Marinho", 1),
     (1234567890, "Spider", "Man", 3),
